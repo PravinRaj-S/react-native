8e905b3933c0d986fd82158ea2768add4e0aac43
PravinRaj-S <pkpravin693@gmail.com>
2025-09-27 11:34:03 +0530

    adding another project for react navigation

---
+++ react-navigation/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    root: true,
+    extends: ['@react-native-community'],
+    rules: {
+        indent: ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+    },
+};
